551c64ea64c7d4b61572bb53cc75e30f5d8b008b
rjsehrlich <racheljsehrlich@gmail.com>
2023-03-29 08:24:54 -0700

    added logic for click event on board

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //console.log('hello new project!')
 
-const player1Input = document.querySelector('#player1-char')
+const player1Input = document.querySelector('#player1-char');
 //console.log(player1Input);
-const player2Input = document.querySelector('#player2-char')
+const player2Input = document.querySelector('#player2-char');
 //console.log(player2Input);
+const tableSquares = document.querySelectorAll('td');
+//console.log(tableSquares);
+
+tableSquares.forEach(el => el.addEventListener('click', () => {
+    console.log('ive been clicked!')
+}))
