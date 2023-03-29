25bf8a6ded08ae1e40743bb9cd6334ff51dbe3d2
rjsehrlich <racheljsehrlich@gmail.com>
2023-03-29 10:37:31 -0700

    logic for displaying current player and switching players

--- script.js
+++ script.js
@@ -6,7 +6,28 @@ const player2Input = document.querySelector('#player2-char');
 //console.log(player2Input);
 const tableSquares = document.querySelectorAll('td');
 //console.log(tableSquares);
+const playerTurn = document.querySelector('#current-player')
+console.log(playerTurn);
+let playerUp = 'Player 1';
 
-tableSquares.forEach(el => el.addEventListener('click', () => {
-    console.log('ive been clicked!')
-}))
+tableSquares.forEach(el => el.addEventListener('click', (event) => {
+    event.preventDefault();
+    //el will refer to the element the event was hooked on
+    let currSquare = el;
+    console.log(el);
+    if (currSquare.className === "") {
+        currSquare.className = playerUp;
+        changePlayers();
+    }
+}));
+
+function changePlayers() {
+    if (playerUp === 'Player 1') {
+        playerUp = 'Player 2';
+        playerTurn.textContent = `Current Player is: ${playerUp}`;
+    } else {
+        playerUp = 'Player 1';
+        playerTurn.textContent = `Current Player is: ${playerUp}`;
+    }
+    playerTurn.className = playerUp;
+}
