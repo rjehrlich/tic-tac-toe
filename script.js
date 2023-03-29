180e65f0c21514b3e88526d4c0124d8c78235cfa
rjsehrlich <racheljsehrlich@gmail.com>
2023-03-29 07:39:50 -0700

    added player inputs, setup variables for js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hello new project!')
+//console.log('hello new project!')
+
+const player1Input = document.querySelector('#player1-char')
+//console.log(player1Input);
+const player2Input = document.querySelector('#player2-char')
+//console.log(player2Input);
