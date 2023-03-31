59760a18020128b3383a39aae3051a9fd7aa4766
rjsehrlich <racheljsehrlich@gmail.com>
2023-03-31 13:03:21 -0700

    updated semantic HTML elements, CSS styling for new color palette

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ const tableSquares = document.querySelectorAll('td');
 const playerTurn = document.querySelector('#current-player');
 //console.log(playerTurn);
 const gameWinner = document.querySelector('#player-win');
-let playerUp = 'player1';
+let playerUp = 'Player1';
+playerTurn.textContent = `Player Up: ${playerUp}`
 
 // const winCombo = [
 //     [0, 1, 2], // top row
@@ -23,53 +24,48 @@ let playerUp = 'player1';
 //   ];
 
 const resetBtn = document.querySelector('#reset-game');
-resetBtn.addEventListener('click', resetGame);
+resetBtn.addEventListener('click', (event) => {
+    //add event listener for reset button to clear out all the class names for td?
+    // tableSquares.forEach(td => {
+    //     td.classList.remove('player1', 'player2');
+    // })
+    // player1Input.value = "X"
+    // player2Input.value = "0"
+    location.reload();
+});
 
 
 tableSquares.forEach(square => square.addEventListener('click', (event) => {
     event.preventDefault();
     //square will refer to the element the event was hooked on
     let currSquare = square;
- 
+
     if (currSquare.className === '') {
         currSquare.classList.add(`${playerUp}`);
         currSquare.innerText = player1Input.value;
     }
-    if (playerUp === 'player2') {
+    if (playerUp === 'Player2') {
         currSquare.classList.add(`${playerUp}`);
         currSquare.innerText = player2Input.value;
     }
     changePlayers();
     checkWinner(tableSquares);
-        //tableArr.push(event.target.id);
-        //console.log(tableArr);
-        //console.log(tableSquares);
-        //checkWinner(winCombo, tableSquares);
 }));
 
 
 
 function changePlayers(e) {
-    if (playerUp === 'player1') {
-        playerUp = 'player2';
-        playerTurn.textContent = `Current Player: ${playerUp}`;
+    if (playerUp === 'Player1') {
+        playerUp = 'Player2';
+        playerTurn.textContent = `Player Up: ${playerUp}`;
     } else {
-        playerUp = 'player1';
-        playerTurn.textContent = `Current Player: ${playerUp}`;
+        playerUp = 'Player1';
+        playerTurn.textContent = `Player Up: ${playerUp}`;
     }
     //playerTurn.className = playerUp;
     checkWinner(tableSquares);
 }
 
-//add event listener for reset button to clear out all the class names for td?
-function resetGame() {
-    tableSquares.forEach(td => {
-        td.classList.remove('player1', 'player2');
-    })
-    player1Input.value = "X"
-    player2Input.value = "0"
-    //location.reload();
-}
 
 //Logic for Winning the Game: compare tableSquares to winners
     //loop through tableSquares & winners
@@ -78,84 +74,84 @@ function resetGame() {
     //if the tablesquare of the matching wincombo contains the class player2
         //player 2 is the winner
 function checkWinner(tableSquares) {
-        if (tableSquares[0].className === 'player1' && 
-            tableSquares[1].className === 'player1' &&
-            tableSquares[2].className === 'player1') {
+        if (tableSquares[0].className === 'Player1' && 
+            tableSquares[1].className === 'Player1' &&
+            tableSquares[2].className === 'Player1') {
             gameWinner.innerText = 'Player 1 Wins!'
         }
-        else if (tableSquares[0].className === 'player2' && 
-            tableSquares[1].className === 'player2' &&
-            tableSquares[2].className === 'player2') {
+        else if (tableSquares[0].className === 'Player2' && 
+            tableSquares[1].className === 'Player2' &&
+            tableSquares[2].className === 'Player2') {
             gameWinner.innerText = 'Player 2 Wins!'
         }
-        if (tableSquares[3].className === 'player1' && 
-            tableSquares[4].className === 'player1' &&
-            tableSquares[5].className === 'player1') {
+        if (tableSquares[3].className === 'Player1' && 
+            tableSquares[4].className === 'Player1' &&
+            tableSquares[5].className === 'Player1') {
             gameWinner.innerText = 'Player 1 Wins!'
         }
-        else if (tableSquares[3].className === 'player2' && 
-            tableSquares[4].className === 'player2' &&
-            tableSquares[5].className === 'player2') {
+        else if (tableSquares[3].className === 'Player2' && 
+            tableSquares[4].className === 'Player2' &&
+            tableSquares[5].className === 'Player2') {
             gameWinner.innerText = 'Player 2 Wins!'
         }
-        if (tableSquares[6].className === 'player1' && 
-            tableSquares[7].className === 'player1' &&
-            tableSquares[8].className === 'player1') {
+        if (tableSquares[6].className === 'Player1' && 
+            tableSquares[7].className === 'Player1' &&
+            tableSquares[8].className === 'Player1') {
             gameWinner.innerText = 'Player 1 Wins!'
         }
-        else if (tableSquares[6].className === 'player2' && 
-            tableSquares[7].className === 'player2' &&
-            tableSquares[8].className === 'player2') {
+        else if (tableSquares[6].className === 'Player2' && 
+            tableSquares[7].className === 'Player2' &&
+            tableSquares[8].className === 'Player2') {
             gameWinner.innerText = 'Player 2 Wins!'
         }
-        if (tableSquares[0].className === 'player1' && 
-            tableSquares[3].className === 'player1' &&
-            tableSquares[6].className === 'player1') {
+        if (tableSquares[0].className === 'Player1' && 
+            tableSquares[3].className === 'Player1' &&
+            tableSquares[6].className === 'Player1') {
             gameWinner.innerText = 'Player 1 Wins!'
         }
-        else if (tableSquares[0].className === 'player2' && 
-            tableSquares[3].className === 'player2' &&
-            tableSquares[6].className === 'player2') {
+        else if (tableSquares[0].className === 'Player2' && 
+            tableSquares[3].className === 'Player2' &&
+            tableSquares[6].className === 'Player2') {
             gameWinner.innerText = 'Player 2 Wins!'
         }
-        if (tableSquares[1].className === 'player1' && 
-            tableSquares[4].className === 'player1' &&
-            tableSquares[7].className === 'player1') {
+        if (tableSquares[1].className === 'Player1' && 
+            tableSquares[4].className === 'Player1' &&
+            tableSquares[7].className === 'Player1') {
             gameWinner.innerText = 'Player 1 Wins!'
         }
-        else if (tableSquares[1].className === 'player2' && 
-            tableSquares[4].className === 'player2' &&
-            tableSquares[7].className === 'player2') {
+        else if (tableSquares[1].className === 'Player2' && 
+            tableSquares[4].className === 'Player2' &&
+            tableSquares[7].className === 'Player2') {
             gameWinner.innerText = 'Player 2 Wins!'
         }
-        if (tableSquares[2].className === 'player1' && 
-            tableSquares[5].className === 'player1' &&
-            tableSquares[8].className === 'player1') {
+        if (tableSquares[2].className === 'Player1' && 
+            tableSquares[5].className === 'Player1' &&
+            tableSquares[8].className === 'Player1') {
             gameWinner.innerText = 'Player 1 Wins!'
         }
-        else if (tableSquares[2].className === 'player2' && 
-            tableSquares[5].className === 'player2' &&
-            tableSquares[8].className === 'player2') {
+        else if (tableSquares[2].className === 'Player2' && 
+            tableSquares[5].className === 'Player2' &&
+            tableSquares[8].className === 'Player2') {
             gameWinner.innerText = 'Player 2 Wins!'
         }
-        if (tableSquares[0].className === 'player1' && 
-            tableSquares[4].className === 'player1' &&
-            tableSquares[8].className === 'player1') {
+        if (tableSquares[0].className === 'Player1' && 
+            tableSquares[4].className === 'Player1' &&
+            tableSquares[8].className === 'Player1') {
             gameWinner.innerText = 'Player 1 Wins!'
         }
-        else if (tableSquares[0].className === 'player2' && 
-            tableSquares[4].className === 'player2' &&
-            tableSquares[8].className === 'player2') {
+        else if (tableSquares[0].className === 'Player2' && 
+            tableSquares[4].className === 'Player2' &&
+            tableSquares[8].className === 'Player2') {
             gameWinner.innerText = 'Player 2 Wins!'
         }
-        if (tableSquares[2].className === 'player1' && 
-            tableSquares[4].className === 'player1' &&
-            tableSquares[6].className === 'player1') {
+        if (tableSquares[2].className === 'Player1' && 
+            tableSquares[4].className === 'Player1' &&
+            tableSquares[6].className === 'Player1') {
             gameWinner.innerText = 'Player 1 Wins!'
         }
-        else if (tableSquares[2].className === 'player2' && 
-            tableSquares[4].className === 'player2' &&
-            tableSquares[6].className === 'player2') {
+        else if (tableSquares[2].className === 'Player2' && 
+            tableSquares[4].className === 'Player2' &&
+            tableSquares[6].className === 'Player2') {
             gameWinner.innerText = 'Player 2 Wins!'
         }
 
