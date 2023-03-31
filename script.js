c89865edff8aa5dddcee46050242fd0aaaa8f10e
rjsehrlich <racheljsehrlich@gmail.com>
2023-03-31 16:31:31 -0700

    refactored the check winner function and added CSS to highlight winning condition

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//console.log('hello new project!')
-
 const player1Input = document.querySelector('#player1-char');
 //console.log(player1Input);
 const player2Input = document.querySelector('#player2-char');
@@ -13,21 +11,12 @@ let playerUp = 'Player1';
 playerTurn.textContent = `Player Up: ${playerUp}`
 const resetBtn = document.querySelector('#reset-game');
 
-// const winCombo = [
-//     [0, 1, 2], // top row
-//     [3, 4, 5], // middle row
-//     [6, 7, 8], // bottom row
-//     [0, 3, 6], // left column
-//     [1, 4, 7], // middle column
-//     [2, 5, 8], // right column
-//     [0, 4, 8], // diag top left to bottom right
-//     [2, 4, 6], // diag top right to bottom left
-//   ];
 
-//add event listener for reset button to clear out all the class names for td?
+//add event listener for reset button to clear out all the class names for td
+    //clear out all updated text that was dependent on game winner
 resetBtn.addEventListener('click', () => {
     tableSquares.forEach(square => {
-        square.classList.remove('Player1', 'Player2');
+        square.classList.remove('Player1', 'Player2', 'winning-square');
         square.innerText = '';
     });
     player1Input.value = "X";
@@ -65,98 +54,42 @@ function changePlayers() {
         playerUp = 'Player1';
         playerTurn.textContent = `Player Up: ${playerUp}`;
     }
-    //playerTurn.className = playerUp;
+
     checkWinner(tableSquares);
 }
 
 
 //Logic for Winning the Game: compare tableSquares to winners
-    //loop through tableSquares & winners
-    //if the tablesquare of the matching wincombo contains the class player1
-        //player 1 is the winner
-    //if the tablesquare of the matching wincombo contains the class player2
-        //player 2 is the winner
+    //loop through wincombos array of arrays
+    //if the tablesquare of the matching wincombo contains the same class
+        //that player of specific class is the winner
+        //update the game result to reflect winner
 function checkWinner(tableSquares) {
-        if (tableSquares[0].className === 'Player1' && 
-            tableSquares[1].className === 'Player1' &&
-            tableSquares[2].className === 'Player1') {        
-            gameWinner.innerText = 'Player 1 Wins!'
-        }
-        else if (tableSquares[0].className === 'Player2' && 
-            tableSquares[1].className === 'Player2' &&
-            tableSquares[2].className === 'Player2') {
-            gameWinner.innerText = 'Player 2 Wins!'
-        }
-        if (tableSquares[3].className === 'Player1' && 
-            tableSquares[4].className === 'Player1' &&
-            tableSquares[5].className === 'Player1') {
-            gameWinner.innerText = 'Player 1 Wins!'
-        }
-        else if (tableSquares[3].className === 'Player2' && 
-            tableSquares[4].className === 'Player2' &&
-            tableSquares[5].className === 'Player2') {
-            gameWinner.innerText = 'Player 2 Wins!'
-        }
-        if (tableSquares[6].className === 'Player1' && 
-            tableSquares[7].className === 'Player1' &&
-            tableSquares[8].className === 'Player1') {
-            gameWinner.innerText = 'Player 1 Wins!'
-        }
-        else if (tableSquares[6].className === 'Player2' && 
-            tableSquares[7].className === 'Player2' &&
-            tableSquares[8].className === 'Player2') {
-            gameWinner.innerText = 'Player 2 Wins!'
-        }
-        if (tableSquares[0].className === 'Player1' && 
-            tableSquares[3].className === 'Player1' &&
-            tableSquares[6].className === 'Player1') {
-            gameWinner.innerText = 'Player 1 Wins!'
-        }
-        else if (tableSquares[0].className === 'Player2' && 
-            tableSquares[3].className === 'Player2' &&
-            tableSquares[6].className === 'Player2') {
-            gameWinner.innerText = 'Player 2 Wins!'
-        }
-        if (tableSquares[1].className === 'Player1' && 
-            tableSquares[4].className === 'Player1' &&
-            tableSquares[7].className === 'Player1') {
-            gameWinner.innerText = 'Player 1 Wins!'
-        }
-        else if (tableSquares[1].className === 'Player2' && 
-            tableSquares[4].className === 'Player2' &&
-            tableSquares[7].className === 'Player2') {
-            gameWinner.innerText = 'Player 2 Wins!'
-        }
-        if (tableSquares[2].className === 'Player1' && 
-            tableSquares[5].className === 'Player1' &&
-            tableSquares[8].className === 'Player1') {
-            gameWinner.innerText = 'Player 1 Wins!'
-        }
-        else if (tableSquares[2].className === 'Player2' && 
-            tableSquares[5].className === 'Player2' &&
-            tableSquares[8].className === 'Player2') {
-            gameWinner.innerText = 'Player 2 Wins!'
-        }
-        if (tableSquares[0].className === 'Player1' && 
-            tableSquares[4].className === 'Player1' &&
-            tableSquares[8].className === 'Player1') {
-            gameWinner.innerText = 'Player 1 Wins!'
-        }
-        else if (tableSquares[0].className === 'Player2' && 
-            tableSquares[4].className === 'Player2' &&
-            tableSquares[8].className === 'Player2') {
-            gameWinner.innerText = 'Player 2 Wins!'
-        }
-        if (tableSquares[2].className === 'Player1' && 
-            tableSquares[4].className === 'Player1' &&
-            tableSquares[6].className === 'Player1') {
-            gameWinner.innerText = 'Player 1 Wins!'
-        }
-        else if (tableSquares[2].className === 'Player2' && 
-            tableSquares[4].className === 'Player2' &&
-            tableSquares[6].className === 'Player2') {
-            gameWinner.innerText = 'Player 2 Wins!'
-        }
-
-
-}
+    const winCombo = [
+        [0, 1, 2], // top row
+        [3, 4, 5], // middle row
+        [6, 7, 8], // bottom row
+        [0, 3, 6], // left column
+        [1, 4, 7], // middle column
+        [2, 5, 8], // right column
+        [0, 4, 8], // diag top left to bottom right
+        [2, 4, 6], // diag top right to bottom left
+    ];
+  
+    for (let i = 0; i < winCombo.length; i++) {
+      const combo = winCombo[i];
+      const square1 = tableSquares[combo[0]];
+      const square2 = tableSquares[combo[1]];
+      const square3 = tableSquares[combo[2]];
+  
+      if (square1.className && square1.className === square2.className && square1.className === square3.className) {
+        gameWinner.innerText = `${square1.className.split(' ')[0]} Wins!`;
+        console.log(square1)
+        square1.classList.add('winning-square');
+        square2.classList.add('winning-square');
+        square3.classList.add('winning-square');
+        return;
+      }
+    }
+  }
+  
