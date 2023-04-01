8cc6219f608d3655292ffc24a12942b7e6062b60
rjsehrlich <racheljsehrlich@gmail.com>
2023-03-31 17:09:48 -0700

    refactored resetting game

--- script.js
+++ script.js
@@ -12,26 +12,14 @@ playerTurn.textContent = `Player Up: ${playerUp}`
 const resetBtn = document.querySelector('#reset-game');
 
 
-//add event listener for reset button to clear out all the class names for td
-    //clear out all updated text that was dependent on game winner
-resetBtn.addEventListener('click', () => {
-    tableSquares.forEach(square => {
-        square.classList.remove('Player1', 'Player2', 'winning-square');
-        square.innerText = '';
-    });
-    player1Input.value = "X";
-    player2Input.value = "0";
-    playerUp = 'Player1';
-    playerTurn.textContent = `Player Up: ${playerUp}`;
-    gameWinner.innerText = '';
-});
-
-
 tableSquares.forEach(square => square.addEventListener('click', (event) => {
     event.preventDefault();
     //square will refer to the element the event was hooked on
     let currSquare = square;
 
+    //check if square has empty class & no text content before marking square
+        //If either not met
+            //the click event will not add new content to the square
     if (currSquare.className === '') {
         currSquare.classList.add(`${playerUp}`);
         currSquare.innerText = player1Input.value;
@@ -84,7 +72,6 @@ function checkWinner(tableSquares) {
   
       if (square1.className && square1.className === square2.className && square1.className === square3.className) {
         gameWinner.innerText = `${square1.className.split(' ')[0]} Wins!`;
-        console.log(square1)
         square1.classList.add('winning-square');
         square2.classList.add('winning-square');
         square3.classList.add('winning-square');
@@ -93,3 +80,19 @@ function checkWinner(tableSquares) {
     }
   }
   
+//add event listener for reset button to clear out all the class names for td
+    //clear out all updated text that was dependent on game winner
+//separate function for resetting the game    
+resetBtn.addEventListener('click', resetGame);
+
+function resetGame() {
+    tableSquares.forEach(square => {
+      square.classList.remove('Player1', 'Player2', 'winning-square');
+      square.innerText = '';
+    });
+    player1Input.value = "X";
+    player2Input.value = "0";
+    playerUp = 'Player1';
+    playerTurn.textContent = `Player Up: ${playerUp}`;
+    gameWinner.innerText = '';
+  }
